4b99b32aa6e8aa0fac7d209e385e8064d9a03a64
Cosimo Lupo <cosimo@anthrotype.com>
2018-10-27 12:09:59 +0100

    Add setup.py script
    I prefer to use setuptools_scm over versioneer.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from io import open
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="wheeltools",
+    use_scm_version={"write_to": "src/wheeltools/_version.py"},
+    description="General tools for working with wheels",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matthew Brett",
+    maintainer="Matthew Brett",
+    author_email="matthew.brett@gmail.com",
+    url="https://github.com/anthrotype/wheeltools",
+    package_dir={"": "src"},
+    packages=find_packages(),
+    install_requires=["wheel"],
+    setup_requires=["setuptools_scm"],
+    license="BSD license",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries :: " "Python Modules",
+        "Topic :: Software Development :: Build Tools",
+    ],
+)
