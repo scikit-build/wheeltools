6a597ec49c58a00d98cd090e157710840d247c3d
Cosimo Lupo <cosimo@anthrotype.com>
2018-10-27 12:00:05 +0100

    Remove all the files not needed by new wheeltools distribution

--- delocate/__init__.py
+++
@@ -1,8 +0,0 @@
-# Init for delocate package
-
-from .delocating import delocate_path, delocate_wheel, patch_wheel
-from .libsana import tree_libs, wheel_libs
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
